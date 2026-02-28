d52b1099b6ad34464d599eaf7a7a98279ae770e2
G-Vatech <laparkaes@gmail.com>
2026-02-27 20:11:07 -0500

    .

---
+++ application/views/home.php
@@ -0,0 +1,18 @@
+<div class="row align-items-end">
+	<div class="col-lg-6">
+		<div class="hero-content-wrapper">
+			<h4 class="wow fadeInUp" data-wow-delay=".2s">You're Using</h4>
+			<h2 class="mb-30 wow fadeInUp" data-wow-delay=".4s">Free Lite Version of Template</h2>
+			<p class="mb-50 wow fadeInUp" data-wow-delay=".6s">Please, purchase full version of the template to get all sections, features and permission to remove footer credit</p>
+			<div class="buttons">
+				<a href="https://rebrand.ly/flat-ud/" rel="nofollow" target="blank" class="button button-lg radius-10 wow fadeInUp" data-wow-delay=".7s">Purchase Now</a>
+			</div>
+		</div>
+	</div>
+	<div class="col-lg-6">
+		<div class="hero-image">
+			<img src="assets/img/hero/hero-2/hero-img.svg" alt="" class="wow fadeInRight" data-wow-delay=".2s">
+			<img src="assets/img/hero/hero-2/paattern.svg" alt="" class="shape shape-1">
+		</div>
+	</div>
+</div>
