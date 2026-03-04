44b90b4a5a0695ee7b36b8bf06253ecb201b1305
G-Vatech <laparkaes@gmail.com>
2026-03-04 18:42:35 -0500

    .

--- application/views/company.php
+++ application/views/company.php
@@ -49,36 +49,51 @@
 
 <section class="section_feature numone_style bg_section">
 	<div class="container">
-        <div class="row align-items-center">
-          <div class="col-lg-6">
-            <div class="numone-image wow fadeInLeft" data-wow-delay=".2s">
-              <img src="<?= base_url() ?>resource/image/numberone.png" alt="number one" />
-            </div>
-          </div>
-          <div class="col-lg-6">
-            <div class="numone-content-wrapper">
-              <div class="section-title mb-40">
-                <h3 class="mb-25 wow fadeInUp" data-wow-delay=".2s">The future of designing starts here</h3>
-                <p class="wow fadeInUp" data-wow-delay=".4s">Stop wasting time and money designing and managing a website that doesn’t get results. Happiness guaranteed, Stop wasting time and money designing and managing a website that doesn’t get results. Happiness guaranteed,</p>
-              </div>
-              <div class="counter-up-wrapper mb-40 wow fadeInUp" data-wow-delay=".6s">
-                <div class="single-counter">
-                  <h4 class="countup" id="secondo1" cup-end="123" cup-append="M">123 M</h4>
-                  <h6>Happy Client</h6>
-                </div>
-                <div class="single-counter">
-                  <h4 class="countup" id="secondo2" cup-end="1434" cup-append="K">1434 K</h4>
-                  <h6>Project Done</h6>
-                </div>
-                <div class="single-counter">
-                  <h4 class="countup" id="secondo3" cup-end="134" cup-append="K">134 K</h4>
-                  <h6>Award Win</h6>
-                </div>
-              </div>
-              <a href="#0" class="button button-lg radius-3 wow fadeInUp" data-wow-delay=".7s">Learn More</a>
-            </div>
-          </div>
-        </div>
+		<div class="row align-items-center">
+			<div class="col-lg-6">
+				<div class="numone-image wow fadeInLeft" data-wow-delay=".2s">
+					<img src="<?= base_url() ?>resource/image/numberone.png" alt="number one" />
+				</div>
+			</div>
+			<div class="col-lg-6">
+				<div class="numone-content-wrapper">
+					<div class="section-title mb-40">
+						<h3 class="mb-25 wow fadeInUp" data-wow-delay=".2s">The future of designing starts here</h3>
+						<p class="wow fadeInUp" data-wow-delay=".4s">Stop wasting time and money designing and managing a website that doesn’t get results. Happiness guaranteed, Stop wasting time and money designing and managing a website that doesn’t get results. Happiness guaranteed,</p>
+						
+						No. 1 en cuota de mercado de rayos X dentales en Corea del Sur.
+
+No. 1 en ventas de equipos de diagnóstico dental 3D a nivel mundial.
+
+No. 1 en participación de mercado mundial de sensores intraorales.
+
+No. 1 en cuota de mercado de rayos X 3D en los Estados Unidos.
+
+No. 1 en preferencia mundial de software de diagnóstico por imágenes dentales.
+
+No. 1 en cuota de mercado de rayos X dentales en el Sudeste Asiático.
+
+No. 1 en cuota de mercado de equipos de rayos X 2D en China.
+						
+						
+					</div>
+					<div class="counter-up-wrapper mb-40 wow fadeInUp" data-wow-delay=".6s">
+						<div class="single-counter">
+							<h4 class="countup" id="secondo1" cup-end="123" cup-append="M">123 M</h4>
+							<h6>Happy Client</h6>
+						</div>
+						<div class="single-counter">
+							<h4 class="countup" id="secondo2" cup-end="1434" cup-append="K">1434 K</h4>
+							<h6>Project Done</h6>
+						</div>
+						<div class="single-counter">
+							<h4 class="countup" id="secondo3" cup-end="134" cup-append="K">134 K</h4>
+							<h6>Award Win</h6>
+						</div>
+					</div>
+				</div>
+			</div>
+		</div>
 	</div>
 </section>
 
