7c3ebd5f3b0d6d0711d233ac61eda8a607bf8936
G-Vatech <laparkaes@gmail.com>
2026-03-08 21:06:17 -0500

    .

--- application/views/distributor.php
+++ application/views/distributor.php
@@ -1,117 +1,162 @@
-	<section class="section_distributor">
-      <div class="container">
-        <div class="row justify-content-center">
-          <div class="col-xxl-7 col-xl-7 col-lg-7 col-md-10">
-            <div class="section-title text-center mb-60">
-              <h3 class="mb-15 wow fadeInUp" data-wow-delay=".2s">Distribuidores</h3>
-              <p class="wow fadeInUp" data-wow-delay=".4s">Vatech, su aliado estratégico en soluciones de radiología dental, está presente muy cerca de usted.</p>
-            </div>
-          </div>
-        </div>
-        
-        <div class="row justify-content-center">
-          <div class="col-xl-3 col-md-6 col-sm-10">
-            <div class="item_distributor wow fadeInUp" data-wow-delay=".2s">
-              <div class="info">
-                <h6>DIGIDENT (H&T MED)</h6>
-				<p class="text-start mt-3"><i class="bi bi-telephone"></i> +51 1 476 2236</p>
-				<p class="text-start"><i class="bi bi-envelope"></i> teo@htdent.com</p>
-				<p class="text-start"><i class="bi bi-envelope"></i> ventas@digident.com.pe</p>
-				<p class="text-start"><i class="bi bi-geo-alt"></i> Cl. Pablo Usandizaga 683, San Borja, Lima</p>
-                <ul class="socials mt-3">
-                  <li>
-                    <a href="https://www.htdent.com" target="_blank"> <i class="bi bi-house"></i> </a>
-                  </li>
-                  <li>
-                    <a href="#0"> <i class="bi bi-facebook"></i> </a>
-                  </li>
-                  <li>
-                    <a href="#0"> <i class="bi bi-instagram"></i> </a>
-                  </li>
-                  <li>
-                    <a href="#0"> <i class="bi bi-whatsapp"></i> </a>
-                  </li>
-                </ul>
-              </div>
-            </div>
-          </div>
-          <div class="col-xl-3 col-md-6 col-sm-10">
-            <div class="item_distributor wow fadeInUp" data-wow-delay=".4s">
-              <div class="image">
-                <img src="assets/img/team/team-1/team-2.png" alt="">
-              </div>
-              <div class="info">
-                <h6>David Endow</h6>
-                <p>Creative Designer</p>
-                <ul class="socials">
-                  <li>
-                    <a href="#0"> <i class="lni lni-facebook-filled"></i> </a>
-                  </li>
-                  <li>
-                    <a href="#0"> <i class="lni lni-twitter-filled"></i> </a>
-                  </li>
-                  <li>
-                    <a href="#0"> <i class="lni lni-instagram-filled"></i> </a>
-                  </li>
-                  <li>
-                    <a href="#0"> <i class="lni lni-linkedin-original"></i> </a>
-                  </li>
-                </ul>
-              </div>
-            </div>
-          </div>
-          <div class="col-xl-3 col-md-6 col-sm-10">
-            <div class="item_distributor wow fadeInUp" data-wow-delay=".6s">
-              <div class="image">
-                <img src="assets/img/team/team-1/team-3.png" alt="">
-              </div>
-              <div class="info">
-                <h6>Jonathon Smith</h6>
-                <p>Brand Designer</p>
-                <ul class="socials">
-                  <li>
-                    <a href="#0"> <i class="lni lni-facebook-filled"></i> </a>
-                  </li>
-                  <li>
-                    <a href="#0"> <i class="lni lni-twitter-filled"></i> </a>
-                  </li>
-                  <li>
-                    <a href="#0"> <i class="lni lni-instagram-filled"></i> </a>
-                  </li>
-                  <li>
-                    <a href="#0"> <i class="lni lni-linkedin-original"></i> </a>
-                  </li>
-                </ul>
-              </div>
-            </div>
-          </div>
-          <div class="col-xl-3 col-md-6 col-sm-10">
-            <div class="item_distributor wow fadeInUp" data-wow-delay=".8s">
-              <div class="image">
-                <img src="assets/img/team/team-1/team-4.png" alt="">
-              </div>
-              <div class="info">
-                <h6>Gray Simon</h6>
-                <p>Frontend Developer</p>
-                <ul class="socials">
-                  <li>
-                    <a href="#0"> <i class="lni lni-facebook-filled"></i> </a>
-                  </li>
-                  <li>
-                    <a href="#0"> <i class="lni lni-twitter-filled"></i> </a>
-                  </li>
-                  <li>
-                    <a href="#0"> <i class="lni lni-instagram-filled"></i> </a>
-                  </li>
-                  <li>
-                    <a href="#0"> <i class="lni lni-linkedin-original"></i> </a>
-                  </li>
-                </ul>
-              </div>
-            </div>
-          </div>
-        </div>
-
-      </div>
-    </section>
+<section class="section_distributor">
+	<div class="container">
+		<div class="row justify-content-center">
+			<div class="col-xxl-7 col-xl-7 col-lg-7 col-md-10">
+				<div class="section-title text-center mb-60">
+					<h3 class="mb-15 wow fadeInUp" data-wow-delay=".2s">Distribuidores</h3>
+					<p class="wow fadeInUp" data-wow-delay=".4s">Vatech, su aliado estratégico en soluciones de radiología dental, está presente muy cerca de usted.</p>
+				</div>
+			</div>
+		</div>
+		<div class="row justify-content-left">
+			<div class="col-xl-3 col-md-6 col-sm-10">
+				<div class="item_distributor wow fadeInUp" data-wow-delay=".2s">
+					<div class="info">
+						<h6 class="mb-3">DIGIDENT (H&T MED)</h6>
+						<div class="d-flex justify-content-start mb-2">
+							<i class="bi bi-telephone me-2"></i>
+							<span class="text-start">+51 1 476 2236</span>
+						</div>
+						<div class="d-flex justify-content-start mb-2">
+							<i class="bi bi-envelope me-2"></i>
+							<span class="text-start">teo@htdent.com</span>
+						</div>
+						<div class="d-flex justify-content-start mb-2">
+							<i class="bi bi-geo-alt me-2"></i>
+							<span class="text-start">Cl. Pablo Usandizaga 683,<br/>San Borja, Lima</span>
+						</div>
+						<ul class="socials mt-3">
+							<li>
+								<a href="https://www.htdent.com" target="_blank"> <i class="bi bi-house"></i> </a>
+							</li>
+							<li>
+								<a href="https://www.facebook.com/htmeddentsac" target="_blank"> <i class="bi bi-facebook"></i> </a>
+							</li>
+							<li>
+								<a href="https://www.instagram.com/htmeddent" target="_blank"> <i class="bi bi-instagram"></i> </a>
+							</li>
+							<li>
+								<a href="https://www.youtube.com/channel/UC0hDbqQceq3Taouih7vOL9g" target="_blank"> <i class="bi bi-youtube"></i> </a>
+							</li>
+						</ul>
+					</div>
+				</div>
+			</div>
+			<div class="col-xl-3 col-md-6 col-sm-10">
+				<div class="item_distributor wow fadeInUp" data-wow-delay=".2s">
+					<div class="info">
+						<h6 class="mb-3">Consultoría Odontológica</h6>
+						<div class="d-flex justify-content-start mb-2">
+							<i class="bi bi-telephone me-2"></i>
+							<span class="text-start">+51 940 479 400</span>
+						</div>
+						<div class="d-flex justify-content-start mb-2">
+							<i class="bi bi-envelope me-2"></i>
+							<span class="text-start">gfiori@ilaeperu.com</span>
+						</div>
+						<div class="d-flex justify-content-start mb-2">
+							<i class="bi bi-geo-alt me-2"></i>
+							<span class="text-start">Av. Ricardo Palma 341, #406,<br/>Miraflores, Lima</span>
+						</div>
+						<ul class="socials mt-3">
+							<li>
+								<a href="https://www.ilaeperu.com/" target="_blank"> <i class="bi bi-house"></i> </a>
+							</li>
+							<li>
+								<a href="https://www.facebook.com/ILAEPERU/" target="_blank"> <i class="bi bi-facebook"></i> </a>
+							</li>
+							<li>
+								<a href="https://www.instagram.com/ilaeperu" target="_blank"> <i class="bi bi-instagram"></i> </a>
+							</li>
+						</ul>
+					</div>
+				</div>
+			</div>
+			<div class="col-xl-3 col-md-6 col-sm-10">
+				<div class="item_distributor wow fadeInUp" data-wow-delay=".2s">
+					<div class="info">
+						<h6 class="mb-3">Cerezza Medical</h6>
+						<div class="d-flex justify-content-start mb-2">
+							<i class="bi bi-telephone me-2"></i>
+							<span class="text-start">+51 989 857 101</span>
+						</div>
+						<div class="d-flex justify-content-start mb-2">
+							<i class="bi bi-envelope me-2"></i>
+							<span class="text-start">cvelasquez@cerezzamedical.com</span>
+						</div>
+						<div class="d-flex justify-content-start mb-2">
+							<i class="bi bi-geo-alt me-2"></i>
+							<span class="text-start">Jr. Tuman 151,<br/>Santiago de Surco, Lima</span>
+						</div>
+						<ul class="socials mt-3">
+							<li>
+								<a href="https://cerezzamedical.com/" target="_blank"> <i class="bi bi-house"></i> </a>
+							</li>
+							<li>
+								<a href="https://www.facebook.com/profile.php?id=100075980823947" target="_blank"> <i class="bi bi-facebook"></i> </a>
+							</li>
+							<li>
+								<a href="https://www.instagram.com/cerezza.medical" target="_blank"> <i class="bi bi-instagram"></i> </a>
+							</li>
+							<li>
+								<a href="https://www.youtube.com/@CerezzaMedical" target="_blank"> <i class="bi bi-youtube"></i> </a>
+							</li>
+						</ul>
+					</div>
+				</div>
+			</div>
+			<div class="col-xl-3 col-md-6 col-sm-10">
+				<div class="item_distributor wow fadeInUp" data-wow-delay=".2s">
+					<div class="info">
+						<h6 class="mb-3">Man Import</h6>
+						<div class="d-flex justify-content-start mb-2">
+							<i class="bi bi-telephone me-2"></i>
+							<span class="text-start">+51 961 235 149</span>
+						</div>
+						<div class="d-flex justify-content-start mb-2">
+							<i class="bi bi-envelope me-2"></i>
+							<span class="text-start">diego.cho75@gmail.com</span>
+						</div>
+						<div class="d-flex justify-content-start mb-2">
+							<i class="bi bi-geo-alt me-2"></i>
+							<span class="text-start">Av. José Pardo 640, #205,<br/>Miraflores, Lima</span>
+						</div>
+						<ul class="socials mt-3">
+							<li>
+								<a href="https://manimport.com.pe" target="_blank"> <i class="bi bi-house"></i> </a>
+							</li>
+							<li>
+								<a href="https://www.facebook.com/sac.manimport" target="_blank"> <i class="bi bi-facebook"></i> </a>
+							</li>
+						</ul>
+					</div>
+				</div>
+			</div>
+			<div class="col-xl-3 col-md-6 col-sm-10">
+				<div class="item_distributor wow fadeInUp" data-wow-delay=".2s">
+					<div class="info">
+						<h6 class="mb-3">GAMBOA GROUP SAC</h6>
+						<div class="d-flex justify-content-start mb-2">
+							<i class="bi bi-telephone me-2"></i>
+							<span class="text-start">+51 991 603 337</span>
+						</div>
+						<div class="d-flex justify-content-start mb-2">
+							<i class="bi bi-envelope me-2"></i>
+							<span class="text-start">gamboagroupcentral@gmail.com</span>
+						</div>
+						<div class="d-flex justify-content-start mb-2">
+							<i class="bi bi-geo-alt me-2"></i>
+							<span class="text-start">Av. Javier Prado Este 1501,<br/>La Victoria, Lima</span>
+						</div>
+						<ul class="socials mt-3">
+							<li>
+								<a href="https://www.instagram.com/gamboagroup" target="_blank"> <i class="bi bi-instagram"></i> </a>
+							</li>
+						</ul>
+					</div>
+				</div>
+			</div>
+		</div>
+	</div>
+</section>
    
