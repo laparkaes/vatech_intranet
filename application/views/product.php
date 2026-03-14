671d2f80d272314bb7701d3bcb72c8145f09a05a
G-Desktop <laparkaes@gmail.com>
2026-03-14 01:38:16 -0500

    .

--- application/views/product.php
+++ application/views/product.php
@@ -12,13 +12,13 @@
 			<div class="col d-flex justify-content-center">
 				<ul class="nav nav-pills mb-3" id="pills-tab" role="tablist">
 					<li class="nav-item" role="presentation">
-						<button class="nav-link active" id="pills-home-tab" data-bs-toggle="pill" data-bs-target="#pills-home" type="button" role="tab" aria-controls="pills-home" aria-selected="true">Home</button>
+						<button class="nav-link radius-30 mx-3 active" id="pills-3d-tab" data-bs-toggle="pill" data-bs-target="#pills-3d" type="button" role="tab" aria-controls="pills-3d" aria-selected="true">Sistema de Imagen 3D</button>
 					</li>
 					<li class="nav-item" role="presentation">
-						<button class="nav-link" id="pills-profile-tab" data-bs-toggle="pill" data-bs-target="#pills-profile" type="button" role="tab" aria-controls="pills-profile" aria-selected="false">Profile</button>
+						<button class="nav-link radius-30 mx-3" id="pills-2d-tab" data-bs-toggle="pill" data-bs-target="#pills-2d" type="button" role="tab" aria-controls="pills-2d" aria-selected="false">Sistema de Imagen 2D</button>
 					</li>
 					<li class="nav-item" role="presentation">
-						<button class="nav-link" id="pills-contact-tab" data-bs-toggle="pill" data-bs-target="#pills-contact" type="button" role="tab" aria-controls="pills-contact" aria-selected="false">Contact</button>
+						<button class="nav-link radius-30 mx-3" id="pills-intra-tab" data-bs-toggle="pill" data-bs-target="#pills-intra" type="button" role="tab" aria-controls="pills-intra" aria-selected="false">Sistema Intraoral</button>
 					</li>
 				</ul>
 			</div>
@@ -26,9 +26,156 @@
 		<div class="row">
 			<div class="col">
 				<div class="tab-content" id="pills-tabContent">
-					<div class="tab-pane fade show active" id="pills-home" role="tabpanel" aria-labelledby="pills-home-tab" tabindex="0">home</div>
-					<div class="tab-pane fade" id="pills-profile" role="tabpanel" aria-labelledby="pills-profile-tab" tabindex="0">profile</div>
-					<div class="tab-pane fade" id="pills-contact" role="tabpanel" aria-labelledby="pills-contact-tab" tabindex="0">contact</div>
+					<div class="tab-pane fade show active" id="pills-3d" role="tabpanel" aria-labelledby="pills-3d-tab" tabindex="0">
+						<div class="row justify-content-center product_portfolio py-3">
+							<div class="col-lg-4 col-md-8 col-sm-10">
+								<div class="single_portfolio">
+									<div class="image">
+										<img src="assets/img/pricing/pricing-1/pricing-1.svg" alt="">
+									</div>
+									<h5>Basic Design</h5>
+									<ul>
+										<li> <i class="lni lni-checkmark-circle"></i> Carefully crafted components</li>
+										<li> <i class="lni lni-checkmark-circle"></i> Amazing page examples</li>
+										<li> <i class="lni lni-checkmark-circle"></i> Super friendly support team</li>
+										<li> <i class="lni lni-checkmark-circle"></i> Awesome Support</li>
+									</ul>
+									<a href="#0" class="button radius-30">Get Started</a>
+								</div>
+							</div>
+							<div class="col-lg-4 col-md-8 col-sm-10">
+								<div class="single_portfolio">
+									<span class="button button-sm radius-30 popular-badge">Best Seller</span>
+									<div class="image">
+										<img src="assets/img/pricing/pricing-1/pricing-2.svg" alt="">
+									</div>
+									<h5>Standard Design</h5>
+									<ul>
+										<li> <i class="lni lni-checkmark-circle"></i> Carefully crafted components</li>
+										<li> <i class="lni lni-checkmark-circle"></i> Amazing page examples</li>
+										<li> <i class="lni lni-checkmark-circle"></i> Super friendly support team</li>
+										<li> <i class="lni lni-checkmark-circle"></i> Awesome Support</li>
+									</ul>
+									<a href="#0" class="button radius-30">Get Started</a>
+								</div>
+							</div>
+							<div class="col-lg-4 col-md-8 col-sm-10">
+								<div class="single_portfolio">
+									<div class="image">
+										<img src="assets/img/pricing/pricing-1/pricing-3.svg" alt="">
+									</div>
+									<h5>Pro Design</h5>
+									<ul>
+										<li> <i class="lni lni-checkmark-circle"></i> Carefully crafted components</li>
+										<li> <i class="lni lni-checkmark-circle"></i> Amazing page examples</li>
+										<li> <i class="lni lni-checkmark-circle"></i> Super friendly support team</li>
+										<li> <i class="lni lni-checkmark-circle"></i> Awesome Support</li>
+									</ul>
+									<a href="#0" class="button radius-30">Get Started</a>
+								</div>
+							</div>
+						</div>
+					</div>
+					<div class="tab-pane fade" id="pills-2d" role="tabpanel" aria-labelledby="pills-2d-tab" tabindex="0">
+						<div class="row justify-content-center product_portfolio py-3">
+							<div class="col-lg-4 col-md-8 col-sm-10">
+								<div class="single_portfolio">
+									<div class="image">
+										<img src="assets/img/pricing/pricing-1/pricing-1.svg" alt="">
+									</div>
+									<h5>Basic Design</h5>
+									<ul>
+										<li> <i class="lni lni-checkmark-circle"></i> Carefully crafted components</li>
+										<li> <i class="lni lni-checkmark-circle"></i> Amazing page examples</li>
+										<li> <i class="lni lni-checkmark-circle"></i> Super friendly support team</li>
+										<li> <i class="lni lni-checkmark-circle"></i> Awesome Support</li>
+									</ul>
+									<a href="#0" class="button radius-30">Get Started</a>
+								</div>
+							</div>
+							<div class="col-lg-4 col-md-8 col-sm-10">
+								<div class="single_portfolio">
+									<span class="button button-sm radius-30 popular-badge">Popular</span>
+									<div class="image">
+										<img src="assets/img/pricing/pricing-1/pricing-2.svg" alt="">
+									</div>
+									<h5>Standard Design</h5>
+									<ul>
+										<li> <i class="lni lni-checkmark-circle"></i> Carefully crafted components</li>
+										<li> <i class="lni lni-checkmark-circle"></i> Amazing page examples</li>
+										<li> <i class="lni lni-checkmark-circle"></i> Super friendly support team</li>
+										<li> <i class="lni lni-checkmark-circle"></i> Awesome Support</li>
+									</ul>
+									<a href="#0" class="button radius-30">Get Started</a>
+								</div>
+							</div>
+							<div class="col-lg-4 col-md-8 col-sm-10">
+								<div class="single_portfolio">
+									<div class="image">
+										<img src="assets/img/pricing/pricing-1/pricing-3.svg" alt="">
+									</div>
+									<h5>Pro Design</h5>
+									<ul>
+										<li> <i class="lni lni-checkmark-circle"></i> Carefully crafted components</li>
+										<li> <i class="lni lni-checkmark-circle"></i> Amazing page examples</li>
+										<li> <i class="lni lni-checkmark-circle"></i> Super friendly support team</li>
+										<li> <i class="lni lni-checkmark-circle"></i> Awesome Support</li>
+									</ul>
+									<a href="#0" class="button radius-30">Get Started</a>
+								</div>
+							</div>
+						</div>
+					</div>
+					<div class="tab-pane fade" id="pills-intra" role="tabpanel" aria-labelledby="pills-intra-tab" tabindex="0">
+						<div class="row justify-content-center product_portfolio py-3">
+							<div class="col-lg-4 col-md-8 col-sm-10">
+								<div class="single_portfolio">
+									<div class="image">
+										<img src="assets/img/pricing/pricing-1/pricing-1.svg" alt="">
+									</div>
+									<h5>Basic Design</h5>
+									<ul>
+										<li> <i class="lni lni-checkmark-circle"></i> Carefully crafted components</li>
+										<li> <i class="lni lni-checkmark-circle"></i> Amazing page examples</li>
+										<li> <i class="lni lni-checkmark-circle"></i> Super friendly support team</li>
+										<li> <i class="lni lni-checkmark-circle"></i> Awesome Support</li>
+									</ul>
+									<a href="#0" class="button radius-30">Get Started</a>
+								</div>
+							</div>
+							<div class="col-lg-4 col-md-8 col-sm-10">
+								<div class="single_portfolio">
+									<span class="button button-sm radius-30 popular-badge">Popular</span>
+									<div class="image">
+										<img src="assets/img/pricing/pricing-1/pricing-2.svg" alt="">
+									</div>
+									<h5>Standard Design</h5>
+									<ul>
+										<li> <i class="lni lni-checkmark-circle"></i> Carefully crafted components</li>
+										<li> <i class="lni lni-checkmark-circle"></i> Amazing page examples</li>
+										<li> <i class="lni lni-checkmark-circle"></i> Super friendly support team</li>
+										<li> <i class="lni lni-checkmark-circle"></i> Awesome Support</li>
+									</ul>
+									<a href="#0" class="button radius-30">Get Started</a>
+								</div>
+							</div>
+							<div class="col-lg-4 col-md-8 col-sm-10">
+								<div class="single_portfolio">
+									<div class="image">
+										<img src="assets/img/pricing/pricing-1/pricing-3.svg" alt="">
+									</div>
+									<h5>Pro Design</h5>
+									<ul>
+										<li> <i class="lni lni-checkmark-circle"></i> Carefully crafted components</li>
+										<li> <i class="lni lni-checkmark-circle"></i> Amazing page examples</li>
+										<li> <i class="lni lni-checkmark-circle"></i> Super friendly support team</li>
+										<li> <i class="lni lni-checkmark-circle"></i> Awesome Support</li>
+									</ul>
+									<a href="#0" class="button radius-30">Get Started</a>
+								</div>
+							</div>
+						</div>
+					</div>
 				</div>
 			</div>
 		</div>
@@ -37,73 +184,5 @@
 	
 	
 	
-	
-	<section id="pricing" class="pricing-section pricing-style-1 bg-white">
-      <div class="container">
-        <div class="row justify-content-center">
-          <div class="col-xxl-5 col-xl-5 col-lg-7 col-md-10">
-            <div class="section-title text-center mb-60">
-              <h3 class="mb-15 wow fadeInUp" data-wow-delay=".2s">Pricing Plan</h3>
-              <p class="wow fadeInUp" data-wow-delay=".4s">Stop wasting time and money designing and managing a website that doesn’t get results. Happiness guaranteed!</p>
-            </div>
-          </div>
-        </div>
 
-        <div class="row justify-content-center">
-          <div class="col-lg-4 col-md-8 col-sm-10">
-            <div class="single-pricing wow fadeInUp" data-wow-delay=".2s">
-              <div class="image">
-                <img src="assets/img/pricing/pricing-1/pricing-1.svg" alt="">
-              </div>
-              <h6>Basic Design</h6>
-              <h4>Web Design</h4>
-              <h3>$ 29.00</h3>
-              <ul>
-                <li> <i class="lni lni-checkmark-circle"></i> Carefully crafted components</li>
-                <li> <i class="lni lni-checkmark-circle"></i> Amazing page examples</li>
-                <li> <i class="lni lni-checkmark-circle"></i> Super friendly support team</li>
-                <li> <i class="lni lni-checkmark-circle"></i> Awesome Support</li>
-              </ul>
-              <a href="#0" class="button radius-30">Get Started</a>
-            </div>
-          </div>
-          <div class="col-lg-4 col-md-8 col-sm-10">
-            <div class="single-pricing active wow fadeInUp" data-wow-delay=".4s">
-              <span class="button button-sm radius-30 popular-badge">Popular</span>
-              <div class="image">
-                <img src="assets/img/pricing/pricing-1/pricing-2.svg" alt="">
-              </div>
-              <h6>Standard Design</h6>
-              <h4>Web Development</h4>
-              <h3>$ 89.00</h3>
-              <ul>
-                <li> <i class="lni lni-checkmark-circle"></i> Carefully crafted components</li>
-                <li> <i class="lni lni-checkmark-circle"></i> Amazing page examples</li>
-                <li> <i class="lni lni-checkmark-circle"></i> Super friendly support team</li>
-                <li> <i class="lni lni-checkmark-circle"></i> Awesome Support</li>
-              </ul>
-              <a href="#0" class="button radius-30">Get Started</a>
-            </div>
-          </div>
-          <div class="col-lg-4 col-md-8 col-sm-10">
-            <div class="single-pricing wow fadeInUp" data-wow-delay=".6s">
-              <div class="image">
-                <img src="assets/img/pricing/pricing-1/pricing-3.svg" alt="">
-              </div>
-              <h6>Pro Design</h6>
-              <h4>Design & Develop</h4>
-              <h3>$ 199.00</h3>
-              <ul>
-                <li> <i class="lni lni-checkmark-circle"></i> Carefully crafted components</li>
-                <li> <i class="lni lni-checkmark-circle"></i> Amazing page examples</li>
-                <li> <i class="lni lni-checkmark-circle"></i> Super friendly support team</li>
-                <li> <i class="lni lni-checkmark-circle"></i> Awesome Support</li>
-              </ul>
-              <a href="#0" class="button radius-30">Get Started</a>
-            </div>
-          </div>
-        </div>
-
-      </div>
-    </section>
    
